92234173bfba2aaf8e09720414f8a65c707dc4f6
birendra7 <birendraraaz620@gmail.com>
2024-12-10 04:50:03 +0530

    operator

---
+++ code/operator.py
@@ -0,0 +1,22 @@
+'''
+Operator Precedence
+ Example: Calculate Simple Interest
+ Take the principal, rate and time from the user and display the simple interest.
+ Operator precedence in Python determines the order in which operators are evaluated in an expression. Here is 
+the precedence of operators in Python, listed in descending order from highest to lowest:
+ 1. Parentheses: ()
+ 2. Exponentiation: **
+ 3. Multiplication, Division, Floor Division, Modulus: *, /, //, %
+ 4. Addition and Subtraction: +, -
+ 5. Bitwise Shifts: <<, >>
+ 6. Bitwise AND: &
+ 7. Bitwise XOR: ^
+ Cracking the Coding Interview in Java  - Foundation
+8. Bitwise OR: |
+ 9. Comparison Operators: ==, !=, >, <, >=, <=, is, is not, in, not in
+ 10. Logical NOT: not
+ 11. Logical AND: and
+ 12. Logical OR: or
+ It is important to note that parentheses can be used to override the default precedence and explicitly define 
+the order of evaluation
+'''
