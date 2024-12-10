494132f274f2a79c5c75415f07ec0bc124e568dc
birendra7 <birendraraaz620@gmail.com>
2024-12-10 12:06:53 +0530

    swap two num using temp varibles

---
+++ code/Example/program4.py
@@ -0,0 +1,14 @@
+#input the variables 
+a = input("Enter the value of the frist varible ")
+b = input("Enter the value of the second varible ")
+
+#dispaly the original values
+print(f"original values : a = {a}, b = {b} ")
+
+#swap the values using the tenp varibles 
+temp = a 
+a = b 
+b = temp
+ #Display the swap values 
+ 
+print(f"swpped values : a ={a }, b = {b}")
